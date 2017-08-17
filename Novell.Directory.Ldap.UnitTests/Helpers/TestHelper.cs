105ffa8d6b35b69ceef9dd295c7d7b30ff84e019
solomon8718 <brian.m.pickens@gmail.com>
2017-08-17 09:04:17 -0500

    Updated Project Structure

---
+++ Novell.Directory.Ldap.UnitTests/Helpers/TestHelper.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Novell.Directory.Ldap.NETStandard.FunctionalTests.Helpers
+{
+    public static class TestHelper
+    {
+        public enum TransportSecurity
+        {
+            Off,
+            Ssl,
+            Tls
+        }
+
+        public static void WithLdapConnection(Action<ILdapConnection> actionOnConnectedLdapConnection, bool useSsl = false, bool disableEnvTransportSecurity = false)
+        {
+            WithLdapConnectionImpl<object>((ldapConnection) =>
+            {
+                actionOnConnectedLdapConnection(ldapConnection);
+                return null;
+            }, useSsl, disableEnvTransportSecurity);
+        }
+
+        public static void WithAuthenticatedLdapConnection(Action<ILdapConnection> actionOnAuthenticatedLdapConnection)
+        {
+            WithLdapConnection(ldapConnection =>
+            {
+                ldapConnection.Bind(TestsConfig.LdapServer.RootUserDn, TestsConfig.LdapServer.RootUserPassword);
+                actionOnAuthenticatedLdapConnection(ldapConnection);
+            });
+        }
+
+        public static T WithLdapConnection<T>(Func<ILdapConnection, T> funcOnConnectedLdapConnection, bool useSsl = false)
+        {
+            return WithLdapConnectionImpl(funcOnConnectedLdapConnection);
+        }
+
+        public static T WithAuthenticatedLdapConnection<T>(Func<ILdapConnection, T> funcOnAuthenticatedLdapConnection)
+        {
+            return WithLdapConnection(ldapConnection =>
+            {
+                ldapConnection.Bind(TestsConfig.LdapServer.RootUserDn, TestsConfig.LdapServer.RootUserPassword);
+                return funcOnAuthenticatedLdapConnection(ldapConnection);
+            });
+        }
+
+        private static T WithLdapConnectionImpl<T>(Func<ILdapConnection, T> funcOnConnectedLdapConnection, bool useSsl = false, bool disableEnvTransportSecurity = false)
+        {
+            using (var ldapConnection = new LdapConnection())
+            {
+                ldapConnection.UserDefinedServerCertValidationDelegate += (sender, certificate, chain, errors) => true;
+                var ldapPort = TestsConfig.LdapServer.ServerPort;
+                var transportSecurity = GetTransportSecurity(useSsl, disableEnvTransportSecurity);
+                if (transportSecurity == TransportSecurity.Ssl)
+                {                    
+                    ldapConnection.SecureSocketLayer = true;
+                    ldapPort = TestsConfig.LdapServer.ServerPortSsl;
+                }
+                ldapConnection.Connect(TestsConfig.LdapServer.ServerAddress, ldapPort);
+
+                T retValue;
+                if (transportSecurity == TransportSecurity.Tls)
+                {
+                    try
+                    {
+                        ldapConnection.StartTls();
+                        retValue = funcOnConnectedLdapConnection(ldapConnection);
+                    }
+                    finally
+                    {
+                        ldapConnection.StopTls();
+                    }
+                }
+                else
+                {
+                    retValue = funcOnConnectedLdapConnection(ldapConnection);
+                }
+                return retValue;
+            }
+        }
+
+        private static TransportSecurity GetTransportSecurity(bool useSsl, bool disableEnvTransportSecurity)
+        {
+            var transportSecurity = useSsl ? TransportSecurity.Ssl : TransportSecurity.Off;
+            if(disableEnvTransportSecurity)
+                return transportSecurity;
+
+            var envValue = Environment.GetEnvironmentVariable("TRANSPORT_SECURITY");
+            if (!string.IsNullOrWhiteSpace(envValue))
+            {
+                TransportSecurity parsedValue;
+                if (Enum.TryParse(envValue, true, out parsedValue))
+                {
+                    transportSecurity = parsedValue;
+                }
+            }
+
+            return transportSecurity;
+        }
+
+        public static string BuildDn(string cn)
+        {
+            return $"cn={cn}," + TestsConfig.LdapServer.BaseDn;
+        }
+    }
+}
---
+++ Novell.Directory.Ldap.UnitTests/VerifyPasswordTests.cs
@@ -0,0 +1,31 @@
+﻿using Novell.Directory.Ldap.NETStandard.FunctionalTests.Helpers;
+using Xunit;
+
+namespace Novell.Directory.Ldap.NETStandard.FunctionalTests
+{
+    public class VerifyPasswordTests
+    {
+        [Fact]
+        public void Bind_ForExistingEntry_ShouldWork()
+        {
+            TestHelper.WithLdapConnection(
+                ldapConnection =>
+                {
+                    ldapConnection.Bind(TestsConfig.LdapServer.RootUserDn, TestsConfig.LdapServer.RootUserPassword);
+                });
+        }
+
+        [Fact]
+        public void Bind_WithWrongPassword_ShouldThrowInvalidCredentials()
+        {
+            var ldapException = Assert.Throws<LdapException>(() =>
+                TestHelper.WithLdapConnection(
+                    ldapConnection =>
+                    {
+                        ldapConnection.Bind(TestsConfig.LdapServer.RootUserDn,
+                            TestsConfig.LdapServer.RootUserPassword + "1");
+                    }));
+            Assert.Equal(LdapException.INVALID_CREDENTIALS, ldapException.ResultCode);
+        }
+    }
+}
